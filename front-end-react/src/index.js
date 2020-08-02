50a9de3d5aaf2911f206464b2f8dc0878e118d3c
Maciag312 <Maciag312>
2020-08-02 23:49:12 +0200

    prepared redux to write

--- front-end-react/src/index.js
+++ front-end-react/src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux'
+
+import rootReducer from './reducers'
+
+
+const store = createStore(rootReducer)
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+        <App/>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
