d0e219f44914cbe97340d9135f6f4ac793ff0cce
Maciag312 <Maciag312>
2020-08-14 14:42:27 +0200

    not tested spring service added almost completed without statistics

---
+++ front-end-react/src/requests.js
@@ -0,0 +1,36 @@
+export const authenticate = (username, password) => {
+    return true; //TODO 
+}
+
+export const addTask = (task) => {
+    return true;//TODO 
+}
+
+
+export const removeTask = (task) => {
+    return true; //TODO 
+}
+
+
+
+export const removeTasks = (task) => {
+    return true;//TODO 
+}
+
+export const GetTasksByDate = (date) => {
+    const task = {duration: 1020123, date: 3210319, isDone: false, name: "first task", category: "cat1"}
+    return task;//TODO 
+    
+}
+
+export const MarkTaskAsDone = (task) => {
+    return true;//TODO 
+}
+ 
+export const UpdateTasks = (tasks) => {
+    return true//TODO 
+}
+
+export const AddTasks = (tasks) => {
+    return true;//TODO 
+}
