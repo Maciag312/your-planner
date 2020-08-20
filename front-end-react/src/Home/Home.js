a4ff2279ac6d2a6172fa052d25f2a20343f95f44
Kamil Bonkowski <bonkowski-kamil@o2.pl>
2020-08-20 18:21:21 +0200

    home visual change

--- front-end-react/src/Home/Home.js
+++ front-end-react/src/Home/Home.js
@@ -6,7 +6,7 @@ export const Home = (props) => {
   return (
     <div className="home">
       <div className="home-box container">
-        <h1 className="home__text">What is your todays goal ? </h1>
+        <h1 className="home__text">What is your todays goal ?</h1>
         <input className="home__input" />
         <br/>
         <button className="home-button ui blue button">Begin adding your tasks</button>
