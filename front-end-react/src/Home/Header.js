414c1672e5545adec93401be1db8b2b8dc1b4002
KamilB00 <bonkowski.kamil@icloud.com>
2020-11-07 00:30:18 +0100

    sidebar and toggel animation done

--- front-end-react/src/Home/Header.js
+++ front-end-react/src/Home/Header.js
@@ -3,57 +3,56 @@ import "./Header.css";
 import logo_white from "./pngs/logo_white.png";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-import { IconButton } from "@material-ui/core";
 import logo_small from "./pngs/logo192.png";
-import Drawer from "./Drawer";
+import { IconContext } from "react-icons";
+
 
 export const Header = (props) => {
   const [show, handleShow] = useState(false);
-  const [drawerOn, setDrawerOn] = useState(false);
-
+  const [sidebar, setSidebar] = useState(false);
   //Listener for effect in navBar
 
-  let history= useHistory()
+  let history = useHistory()
 
- 
+  const showSidebar = () => setSidebar(!sidebar);
 
-  const  buttonLook = makeStyles({
+  const buttonLook = makeStyles({
     buttonSignUp: {
       backgroundColor: 'white',
       color: '#0087FF',
       height: '30px',
-     textTransform: 'none',
-     '&:hover':{
-      backgroundColor: 'white',
-      color: '#0087FF',
-      boxShadow: "0px 0px 50px",
-      transition: "0.3s color",
-      
-    },
-    
-  }})
+      textTransform: 'none',
+      '&:hover': {
+        backgroundColor: 'white',
+        color: '#0087FF',
+        boxShadow: "0px 0px 50px",
+        transition: "0.3s color",
+
+      },
+
+    }
+  })
 
   const buttonLookFunctions = makeStyles({
     buttonFunctions: {
       backgroundColor: 'transparent',
       color: 'white',
       height: '100%',
-     textTransform: 'none',
-     boxShadow: "none",
-     '&:hover':{
-      color: '#1E58FF',
-      display: 'inline',
-    },
+      textTransform: 'none',
+      boxShadow: "none",
+      '&:hover': {
+        color: '#1E58FF',
+        display: 'inline',
+      },
     },
   })
- 
+
   const classes = buttonLook();
- 
+
   const classes3 = buttonLookFunctions();
-  
+
   useEffect(() => {
     window.onscroll = () => {
       if (window.scrollY > 100) {
@@ -63,75 +62,80 @@ export const Header = (props) => {
   }, []);
 
   return (
-      
-    <div className={`header ${show && 'header_scroll'} `}>
-      {/* logo  -->left*/}
-      <div className="header_logo_box">
-      <Link to="/">
-        <img className={"header__logo"} src={logo_white} alt="" />
-        <img className={"header__logo__small"} src={logo_small} alt="" />
-      </Link>
-      </div>
+    <>
+    <IconContext.Provider value={{color: '#fff'}} >
+    <div className={sidebar ? `header_navbar open` : 'header_navbar'}>
 
-      {/* Header blank box*/}
-
-      <div className ="header__navBox__after__logo">
-      <div className="header__nav">
-        <Link className={classes3.buttonFunctions}  >
-        <div className="header__option">
-             Functions
-            </div>
-      </Link>
-      </div>
-      
-      <div className="header__nav">
-        <Link className={classes3.buttonFunctions}  >
-        <div className="header__option">
-             About us
-            </div>
-      </Link>
-      </div>
-      </div>
-
-      <div className="header__navBox">
-
-
-        {/* 1st Link  login */}
-        <div className="header__nav">
-          <Link
-          
-            className={classes3.buttonFunctions}
-            onClick={()=>{history.push("/login")}}
-          >
-            <div className="header__option__registration">
-              Log in
-            </div>
-          
+    
+    <div className={sidebar ? `header_logo_box open` : 'header_logo_box'}>
+          <Link to="/">
+            <img className={"header__logo"} src={logo_white} alt="" />
+            <img className={"header__logo__small"} src={logo_small} alt="" />
           </Link>
         </div>
-        {/* 2nd Link  Registration  --> right*/}
-        <div className="header__nav">
-          <Button
-            className={classes.buttonSignUp}
-            disableRipple={true}
-            onClick={()=>{history.push("/registration")}}
-          >
-            <div className="header__option__registration">
-               Sign up
-            </div>
-          </Button>
-          
-        </div>
+
+      <div onClick={()=>showSidebar()} className={sidebar ? `menu-btn open` : 'menu-btn'}>
+      <div className="menu-btn__burger"></div>
       </div>
-        <div className="menu-icon"> 
-        <IconButton disableRipple={true} onClick={()=> {setDrawerOn(!drawerOn)}}>
-          <MenuIcon fontSize='large' style={{color: "white"}}/>
-        </IconButton>
+        
+      <div className={sidebar ? 'header active' : `header ${show && 'header_scroll'}`}>
+        
+       </div>
+
+
+        {/* Links: Functions, AboutUs  */}
+
+        <div className={sidebar ? `header__navBox__after__logo open` : 'header__navBox__after__logo'}>
+          <div className="header__nav">
+            <Link className={classes3.buttonFunctions}  >
+              <div className="header__option">
+                Functions
+            </div>
+            </Link>
+          </div>
+
+          <div className="header__nav">
+            <Link className={classes3.buttonFunctions}>
+              <div className="header__option">
+                About us
+            </div>
+            </Link>
+          </div>
+
         </div>
-        <Drawer drawerOn={drawerOn} onDrawerChange={setDrawerOn} />
-    </div>
-  
-    
+
+        {/* Link LogIn, Button SignUp */}
+        <div className={sidebar ? `header__navBox open` : 'header__navBox'}>
+          {/*  Link  login */}
+          <div className={sidebar ? `header__nav open` : 'header__nav'}>
+            <Link
+              className={classes3.buttonFunctions}
+              onClick={() => { history.push("/login") }}
+            >
+              <div className="header__option__registration">
+                Log in
+            </div>
+            </Link>
+          </div>
+
+          <div className="header__nav">
+            <Button
+              className={classes.buttonSignUp}
+              disableRipple={true}
+              onClick={() => { history.push("/registration") }}
+            >
+              <div className="header__option__registration">
+                Sign up
+            </div>
+            </Button>
+          </div>
+        </div>
+
+      </div>
+      </IconContext.Provider>
+
+
+    </>
   );
 };
 
---
+++ front-end-react/src/Home/SidebarData.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+
+export const SidebarData = [
+{
+    title: 'Functions',
+    path: '/functions',
+    className: 'nav-text'
+},
+{
+    title: 'About us',
+    path: '/aboutus',
+    className: 'nav-text'
+},
+{
+    title: 'Contact',
+    path: '/contact',
+    className: 'nav-text'
+},
+{
+    title: 'Support',
+    path: '/support',
+    className: 'nav-text'
+},
+{
+    title: 'Login',
+    path: '/login',
+    className: 'nav-login-button'
+},
+{
+    title: 'Sign Up',
+    path: '/registration',
+    className: 'nav-signup-button'
+},
+
+]
+
